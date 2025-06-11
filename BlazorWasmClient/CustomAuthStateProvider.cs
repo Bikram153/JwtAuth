9b2d40d36e157d474686597d319e45da06cd8769
Bikram153 <samantabikramviktar@gmail.com>
2025-06-11 13:52:40 +0530

    frontend blazor added

---
+++ BlazorWasmClient/CustomAuthStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorWasmClient
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyAuthenticationStateChanged() =>
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+}
